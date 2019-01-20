e8311714bebc15d5d100e68e693d2f1d5dd5180e
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-01-20 19:24:11 -0200

    Updates

---
+++ precache-manifest.bf0a8db339bdfd08998693f3110f440e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2320353eb56f5d9b24d157b1c6a33d0",
+    "url": "/static/media/profile_picture.a2320353.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2babfbe2f2b1fcef8650",
+    "url": "/static/js/main.2babfbe2.chunk.js"
+  },
+  {
+    "revision": "9fac4be24baf7918d832",
+    "url": "/static/js/1.9fac4be2.chunk.js"
+  },
+  {
+    "revision": "2babfbe2f2b1fcef8650",
+    "url": "/static/css/main.40bdc170.chunk.css"
+  },
+  {
+    "revision": "52e109a270a010f78f889ed1743f5a20",
+    "url": "/index.html"
+  }
+];
--- src/Theme.js
+++
@@ -1,9 +0,0 @@
-export const Primary = '#000'
-
-export const Secondary = '#292F36'
-
-export const White = '#F7FFF7'
-
-export const Accent = '#00FFC5'
-
-export const SecondaryAccent = '#1B2A41'
